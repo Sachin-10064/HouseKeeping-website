28ae0099a2d638022f5f9294be772d96e6446bf9
Sachin-10064 <sachinyadav10064@gmail.com>
2021-07-18 00:05:57 +0530

    add server

--- housekeepingwebportel/settings.py
+++ housekeepingwebportel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ueft*ksj2h5xm*m!x)_5adsmdzq9e2q5y*mg&s0w=y3n%+i%#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','housekeepingwebsite.herokuapp.com']
 
 
 # Application definition
